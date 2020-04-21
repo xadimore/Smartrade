7050f4672b1c87f143c2cc0a255f887dd92e8f5b
Olasunkanmi Ariyibi <xadimore@yahoo.com>
2020-04-21 19:37:26 +0100

    Add project files.

---
+++ Smartrade/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Smartrade.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
